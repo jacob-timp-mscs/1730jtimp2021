d2c5f6a4aa3a63f761f09094b8600f7d50fa9f75
Jacob Timp <jacob.timp@my.southeastmn.edu>
2021-09-05 23:26:28 -0500

    Completed Ex1C

---
+++ jtimp1730ex1c1/frmFoodTruck.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jtimp1730ex1c1
+{
+    public partial class frmFoodTruck : Form
+    {
+        public frmFoodTruck()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtHotDogsSubtotal.Text = (
+                4.0m * Convert.ToDecimal(txtHotDogs.Text)
+                ).ToString("0.00");
+
+            txtHamburgersSubtotal.Text = (
+                5.0m * Convert.ToDecimal(txtHamburgers.Text)
+                ).ToString("0.00");
+
+            txtPreTaxTotal.Text = (
+                Convert.ToDecimal(txtHotDogsSubtotal.Text) + Convert.ToDecimal(txtHamburgersSubtotal.Text)
+
+                ).ToString("0.00");
+
+            txtTax.Text = (
+            0.06875m * Convert.ToDecimal(txtPreTaxTotal.Text)
+                ).ToString("0.00");
+
+            txtTotal.Text = (
+                Convert.ToDecimal(txtPreTaxTotal.Text) + Convert.ToDecimal(txtTax.Text)
+                ).ToString("0.00");
+
+            btnClear.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHotDogs.Text = "0";
+            txtHamburgers.Text = "0";
+            txtHotDogsSubtotal.Text = "";
+            txtHamburgersSubtotal.Text = "";
+            txtPreTaxTotal.Text = "";
+            txtTax.Text = "";
+            txtTotal.Text = "";
+
+            txtHotDogs.Focus();
+          
+        }
+    }
+}
